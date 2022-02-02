d2b37dff2491f6dba3a026dd0a2b26890d653536
RISHABH MISHRA <rish.mish.2607@gmail.com>
2022-02-03 02:35:43 +0530

    feat: added dark theme

---
+++ js/theme.js
@@ -0,0 +1,20 @@
+const themeSwitch = document.querySelector(".theme-switch");
+
+function switchTheme(e) {
+  var theme = document.documentElement.getAttribute("data-theme");
+  if (e.target) {
+    if (theme == "dark") {
+      document.documentElement.setAttribute("data-theme", "light");
+      themeSwitch.classList.remove("fa-sun");
+      themeSwitch.classList.add("fa-moon");
+    } else {
+      document.documentElement.setAttribute("data-theme", "dark");
+      themeSwitch.classList.remove("fa-moon");
+      themeSwitch.classList.add("fa-sun");
+    }
+  }
+  //   console.log(theme);
+}
+
+themeSwitch.addEventListener("click", switchTheme, false);
+// themeSwitch.addEventListener("click", () => console.log("clicked"));
